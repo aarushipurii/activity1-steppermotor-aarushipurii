2ecf670e72fa877fdbbc6f528f4b515c4e11dd69
aarushipurii <aarushi.puri2025@bitsdesign.edu.in>
2026-02-18 15:17:11 +0530

    Add GPIO control loop for LED sequence

--- Code1.py
+++ Code1.py
@@ -1,1 +1,20 @@
+from machine import Pin
+import time
 
+list1 = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
+in1 = Pin(14, Pin.OUT)
+in2 = Pin(25, Pin.OUT)
+in3 = Pin(26, Pin.OUT)
+in4 = Pin(27, Pin.OUT)
+
+sleep = 0.003
+
+while True:
+    for i in list1:
+        in1.value(i[0])
+        in2.value(i[1])
+        in3.value(i[2])
+        in4.value(i[3])
+        
+        time.sleep(sleep)
